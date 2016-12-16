5c053458b2fff184d68b40af67f40ebab2494fd4
matteo <matteo@falampi>
2016-12-16 12:25:43 +0100

    aggiornato managepaper

--- assets/conferences/managepapers.controller.js
+++ assets/conferences/managepapers.controller.js
@@ -1,25 +1,52 @@
 app.controller('managepapersController',
-    ($scope, $http, $location, $routeParams) => {
-	  $scope.conf = {id: $routeParams.cid}
-	  $http.post('/conference/getData', $scope.conf).then(res => {
-    $scope.conf = res.data.conference
-	    $scope.conf.paperstate = '' + res.data.conference.status
-    $http.post('/conference/papers', $scope.conf).then(res => {
-      $scope.conf.papers = res.data.papers
-    })
-	  })
-	  $scope.submit = function () {
-    $http.post('/conference/create', $scope.conf).then(res => {
-		  $location.path('/conferences/' + res.data.conference.id + '/addchairs')
-    })
-  }
-	  $scope.setStatus = function () {
-	    $scope.req = {id: $scope.conf.id, state: $scope.conf.paperstate}
-	    $http.post('/conference/setStatus', $scope.req)
-	  }
-	  $scope.close = function () {
-	    $scope.req = {id: $scope.conf.id, state: 2}
-	    $http.post('/conference/setStatus', $scope.req)
-	  }
+  ($scope, $http, $location, $routeParams) => {
+    $scope.conf = {
+      id: $routeParams.cid
     }
-  )
+    $http.post('/conference/getData', $scope.conf).then(res => {
+      $scope.conf = res.data.conference
+      $scope.conf.paperstate = '' + res.data.conference.status
+      $http.post('/conference/papers', $scope.conf).then(res => {
+        $scope.conf.papers = {
+          accepted: [],
+          pending: [],
+          rejected: []
+        }
+        res.data.papers.forEach((el) => {
+          switch (el.status) {
+            case 0:
+              $scope.conf.papers.pending.push(el)
+              break
+            case 1:
+              $scope.conf.papers.accepted.push(el)
+              break
+            case 2:
+              $scope.conf.papers.rejected.push(el)
+              break
+            default:
+              break
+          }
+        })
+      })
+    })
+    $scope.submit = function () {
+      $http.post('/conference/create', $scope.conf).then(res => {
+        $location.path('/conferences/' + res.data.conference.id + '/addchairs')
+      })
+    }
+    $scope.setStatus = function () {
+      $scope.req = {
+        id: $scope.conf.id,
+        state: $scope.conf.paperstate
+      }
+      $http.post('/conference/setStatus', $scope.req)
+    }
+    $scope.close = function () {
+      $scope.req = {
+        id: $scope.conf.id,
+        state: 2
+      }
+      $http.post('/conference/setStatus', $scope.req)
+    }
+  }
+)
