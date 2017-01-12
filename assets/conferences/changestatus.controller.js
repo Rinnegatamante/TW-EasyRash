4756862fe8c93863c9574d81e4ab8e32c686e930
matteo <matteo@falampi>
2017-01-12 23:23:22 +0100

    aggiunta vista ai file rush, per ora è solo momentanea e non ci sono controlli, è solo per andare avanti con le funzioni dei commenti. In seguito sarà disponibile solo una volta che il paper è stato accettato. Ora è visibile solo per i paper che si possiedono e raggiungibile tramite il pulsante in committe to:.

---
+++ assets/conferences/changestatus.controller.js
@@ -0,0 +1,67 @@
+app.controller('changestatusController',
+<<<<<<< HEAD
+  ($scope, $http, $location, $routeParams) => {
+    $scope.conf = {
+      id: $routeParams.cid
+    }
+    $http.post('/conference/getData', $scope.conf).then(res => {
+      $scope.conf = res.data.conference
+      $scope.conf.paperstate = '' + res.data.conference.status
+      $http.post('/conference/papers', $scope.conf).then(res => {
+        $scope.conf.pending_papers = []
+        res.data.papers.forEach((el) => {
+          if (el.status == 0) $scope.conf.pending_papers.push(el)
+        })
+      })
+    })
+    $scope.submit = function () {
+      $http.post('/conference/create', $scope.conf).then(res => {
+        $location.path('/conferences/' + res.data.conference.id + '/addchairs')
+      })
+    }
+    $scope.setStatus = function () {
+      $scope.req = {
+        id: $scope.conf.id,
+        state: $scope.conf.paperstate
+      }
+      $http.post('/conference/setStatus', $scope.req)
+    }
+    $scope.close = function () {
+      $scope.req = {
+        id: $scope.conf.id,
+        state: 2
+      }
+      $http.post('/conference/setStatus', $scope.req)
+    }
+  }
+=======
+	($scope, $http, $location, $routeParams) => {
+		$scope.conf = {id: $routeParams.cid}
+		$http.post('/conference/getData', $scope.conf).then(res => {
+			$scope.conf = res.data.conference
+			$scope.conf.paperstate = '' + res.data.conference.status
+			$http.post('/conference/papers', $scope.conf).then(res => {
+				$scope.conf.papers = {
+					pending: []
+				}
+				res.data.papers.forEach((el) => {
+					if (el.status == 0 || el.status == 3) $scope.conf.papers.pending.push(el)
+				})
+			})
+		})
+		$scope.submit = function () {
+			$http.post('/conference/create', $scope.conf).then(res => {
+				$location.path('/conferences/' + res.data.conference.id + '/addchairs')
+			})
+		}
+		$scope.setStatus = function () {
+			$scope.req = {id: $scope.conf.id, state: $scope.conf.paperstate}
+			$http.post('/conference/setStatus', $scope.req)
+		}
+		$scope.close = function () {
+			$scope.req = {id: $scope.conf.id, state: 2}
+			$http.post('/conference/setStatus', $scope.req)
+		}
+    }
+>>>>>>> ce57c5834bb1c5e51bfebaebe3409caf5ae0eb9d
+)
