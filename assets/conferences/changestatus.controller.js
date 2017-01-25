e90a230b85a6b6e93af78eaa1d26225126574c8e
Rinnegatamante <rinnegatamante@gmail.com>
2017-01-25 11:47:02 +0100

    Removing Git versioning text.
    Removing Git versioning text.

--- assets/conferences/changestatus.controller.js
+++ assets/conferences/changestatus.controller.js
@@ -1,5 +1,4 @@
 app.controller('changestatusController',
-// <<<<<<< HEAD
   ($scope, $http, $location, $routeParams) => {
     $scope.conf = {
       id: $routeParams.cid
@@ -34,34 +33,4 @@ app.controller('changestatusController',
       $http.post('/conference/setStatus', $scope.req)
     }
   }
-/* =======
-	($scope, $http, $location, $routeParams) => {
-		$scope.conf = {id: $routeParams.cid}
-		$http.post('/conference/getData', $scope.conf).then(res => {
-			$scope.conf = res.data.conference
-			$scope.conf.paperstate = '' + res.data.conference.status
-			$http.post('/conference/papers', $scope.conf).then(res => {
-				$scope.conf.papers = {
-					pending: []
-				}
-				res.data.papers.forEach((el) => {
-					if (el.status == 0 || el.status == 3) $scope.conf.papers.pending.push(el)
-				})
-			})
-		})
-		$scope.submit = function () {
-			$http.post('/conference/create', $scope.conf).then(res => {
-				$location.path('/conferences/' + res.data.conference.id + '/addchairs')
-			})
-		}
-		$scope.setStatus = function () {
-			$scope.req = {id: $scope.conf.id, state: $scope.conf.paperstate}
-			$http.post('/conference/setStatus', $scope.req)
-		}
-		$scope.close = function () {
-			$scope.req = {id: $scope.conf.id, state: 2}
-			$http.post('/conference/setStatus', $scope.req)
-		}
-    }
->>>>>>> ce57c5834bb1c5e51bfebaebe3409caf5ae0eb9d */
 )
