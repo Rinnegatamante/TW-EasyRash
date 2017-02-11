bd143293fb6c1c25a8a941e416bb8f27f98cbeda
Rinnegatamante <rinnegatamante@gmail.com>
2017-02-11 12:51:15 +0100

    Fixed filter for papers counting.

--- assets/conferences/reviewer.controller.js
+++ assets/conferences/reviewer.controller.js
@@ -1,11 +1,13 @@
-app.filter('checkId', function() {
-	return function(input) {
-		if (input == 1) return input;
-		else return input;
-	}
-}).controller('reviewerController',
+app.controller('reviewerController',
  ($scope, $http, $routeParams, $location) => {
    $http.post('/user/getdata').then(res => {
      $scope.user = res.data.user
    })
- });
+  $scope.filterPapers = function(conference){
+	return function(paper){
+		console.log(paper);
+		console.log(conference);
+		return conference == paper.conference;
+	}
+  }
+});
