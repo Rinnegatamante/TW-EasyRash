eb967fa195562dc909c36262ceec7654132898c6
Rinnegatamante <rinnegatamante@gmail.com>
2017-02-13 14:44:44 +0100

    Identata e commentata l'intera cartella register.

--- assets/register/register.component.js
+++ assets/register/register.component.js
@@ -1,14 +1,16 @@
-app.controller('registerController',
-    ($scope, $http, $location) => {
-	  $scope.user = {sex: 'Male'}
-      $scope.submit = function () {
-        if ($scope.user.password === $scope.user.password2) {
-          $http.post('/user/register', $scope.user).then(res => {
-            $location.path('/login')
-	       })
-        } else {
-          alertify.error('Password mismatches.')
-        }
-      }
-    }
-  )
+// Controller for register template
+app.controller('registerController',($scope, $http, $location) => {
+	$scope.user = {sex: 'Male'} // Setting default gender
+	
+	// submit function, insert new user data to the database
+	$scope.submit = function () {
+		if ($scope.user.password === $scope.user.password2) {
+			$http.post('/user/register', $scope.user).then(res => {
+				$location.path('/login')
+			})
+		} else {
+			alertify.error('Password mismatches.')
+		}
+	}
+	
+})
