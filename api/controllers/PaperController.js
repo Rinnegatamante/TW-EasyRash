a678181ac02bc17300449c97b833999f34a4b925
Rinnegatamante <rinnegatamante@gmail.com>
2017-02-13 17:03:23 +0100

    Identati e commentati i controller di sails.

--- api/controllers/PaperController.js
+++ api/controllers/PaperController.js
@@ -1,194 +1,226 @@
 /**
  * PaperController
  *
- * @description :: Server-side logic for managing files
+ * @description :: Server-side logic for managing papers
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
 module.exports = {
-  _config: {
-    actions: false,
-    shortcuts: false,
-    rest: false
-  },
+	
+	_config: {
+		actions: false,
+		shortcuts: false,
+		rest: false
+	},
+	
+	// find: Searches for a paper on the database
+	find: function(req, res) {
+		Paper.findOne(req.param('pid')).populate('conference').populate('author').populate('owner')
+			.populate('reviewers').populate('reviews').exec((err, paper) => {
+				if (err) {
+					console.log(err)
+				}
+				return res.json({
+					paper: paper
+				})
+			})
+	},
 
-  index: function (req, res) {
-    return res.views('paper_up')
-  },
+	// accept: Sets a paper to accepted status
+	accept: function(req, res) {
+		Paper.findOne(req.param('pid')).exec((err, paper) => {
+			paper.status = 1
+			paper.save((err) => {
+				if (err) {
+					console.log(err)
+				}
+			})
+			return res.json({
+				message: 'Paper status changed successfully!',
+				paper: paper
+			})
+		})
+	},
 
-  find: function (req, res) {
-    Paper.findOne(req.param('pid')).populate('conference').populate('author').populate('owner')
-    .populate('reviewers').populate('reviews').exec((err, paper) => {
-      if (err) { console.log(err) }
-      return res.json({
-        paper: paper
-      })
-    })
-  },
+	// reject: Sets a paper to rejected status
+	reject: function(req, res) {
+		Paper.findOne(req.param('pid')).exec((err, paper) => {
+			paper.status = 2
+			paper.save((err) => {
+				if (err) {
+					console.log(err)
+				}
+			})
+			return res.json({
+				message: 'Paper status changed successfully!',
+				paper: paper
+			})
+		})
+	},
 
-  pending: function (req, res) {
-    Paper.findOne(req.param('pid')).exec((err, paper) => {
-      paper.status = 0
-      paper.save((err) => {
-        if (err) { console.log(err) }
-      })
-      return res.json({
-        message: 'Saved',
-        paper: paper
-      })
-    })
-  },
+	// delete: Removes a paper from the database
+	delete: function(req, res) {
+		Paper.findOne(req.param('pid')).exec((err, paper) => {
+			if (err) {
+				return res.negotiate(err)
+			}
+			var path = paper.url
+			Paper.destroy({
+				id: req.param('pid')
+			}).exec((err, paper) => {
+				if (err) {
+					return res.negotiate(err)
+				}
+				const fs = require('fs')
 
-  accept: function (req, res) {
-    Paper.findOne(req.param('pid')).exec((err, paper) => {
-      paper.status = 1
-      paper.save((err) => {
-        if (err) { console.log(err) }
-      })
-      return res.json({
-        message: 'Saved',
-        paper: paper
-      })
-    })
-  },
+				fs.unlink(path, (err) => {
+					if (err) throw err
+				})
+				return res.json({
+					message: 'Paper deleted successfully!'
+				})
+			})
+		})
+	},
 
-  reject: function (req, res) {
-    Paper.findOne(req.param('pid')).exec((err, paper) => {
-      paper.status = 2
-      paper.save((err) => {
-        if (err) { console.log(err) }
-      })
-      return res.json({
-        message: 'Saved ',
-        paper: paper
-      })
-    })
-  },
+	// upload: Uploads a paper to the dataabase
+	upload: function(req, res) {
+		if (!req.param('cid')) return res.json(400, {
+			message: 'Conference field is empty.'
+		})
+		var co_ids = req.param('co_ids') ? req.param('co_ids').split(',') : []
+		var u = AuthService.user()
+		co_ids.push(u.id)
 
-  delete: function (req, res) {
-    Paper.findOne(req.param('pid')).exec((err, paper) => {
-      if (err) { return res.negotiate(err) }
-      var path = paper.url
-      Paper.destroy({id: req.param('pid')}).exec((err, paper) => {
-        if (err) { return res.negotiate(err) }
-        const fs = require('fs')
+		req.file('file').upload({
+			maxBytes: 10000000,
+			dirname: require('path').resolve(sails.config.appPath, 'assets/uploads/')
+		}, function(err, files) {
+			if (err) {
+				return res.serverError(err)
+			}
+			if (files.length == 0) return res.json(400, {
+				message: 'File field is empty.'
+			})
+			for (var i = 0; files[i]; i++) {
+				Paper.create({
+					title: files[i].filename,
+					mime: files[i].type,
+					url: files[i].fd,
+					conference: req.param('cid'),
+					author: co_ids
+				}).exec(function(err, createdFile) {
 
-        fs.unlink(path, (err) => {
-          if (err) throw err
-        })
-        return res.json({
-          message: 'Paper deleted successfully'
-        })
-      })
-    })
-  },
+					u.papers.add(createdFile.id)
+					createdFile.save()
 
-  upload: function (req, res) {
-    if (!req.param('cid')) return res.json(400, {message: 'Conference field is empty.'})
-    var co_ids = req.param('co_ids') ? req.param('co_ids').split(',') : []
-    var u = AuthService.user()
-    co_ids.push(u.id)
+					if (!files[i + 1]) {
+						u.save(function(err) {
+							if (err) {
+								console.log(err)
+							}
 
-    req.file('file').upload({
-      maxBytes: 10000000,
-      dirname: require('path').resolve(sails.config.appPath, 'assets/uploads/')
-    }, function (err, files) {
-      if (err) {
-        return res.serverError(err)
-      }
-      if (files.length == 0) return res.json(400, {message: 'File field is empty.'})
-      console.log('FILES', files)
-      for (var i = 0; files[i]; i++) {
-        Paper.create({
-          title: files[i].filename,
-          mime: files[i].type,
-          url: files[i].fd,
-          conference: req.param('cid'),
-          author: co_ids
-        }).exec(function (err, createdFile) {
-          if (err) { console.log(err) }
+							return res.json({
+								message: files.length + ' file(s) uploaded successfully!',
+								attributes: createdFile.toObject(),
+								paper: files
+							})
+						})
+					}
+				})
+			}
+		})
+	},
 
-          u.papers.add(createdFile.id)
+	// addAuthor: Adds an author to a paper
+	addAuthor: function(req, res) {
+		if (!req.param('aid')) return res.json(400, {
+			message: 'Author is not specified.'
+		})
+		Paper.findOne(req.param('pid')).populate('author').exec((err, paper) => {
+			if (err) {
+				return res.negotiate(err)
+			}
+			paper.author.add(req.param('aid'))
+			paper.save(function(err) {
+				if (err) {
+					console.log(err)
+				}
 
-          console.log(createdFile)
-          createdFile.save()
+				return res.json({
+					message: 'Author added successfully!',
+					paper: paper
+				})
+			})
+		})
+	},
+	
+	// removeAuthor: Removes an author from a paper
+	removeAuthor: function(req, res) {
+		if (!req.param('aid')) return res.json(400, {
+			message: 'Author is not specified.'
+		})
+		Paper.findOne(req.param('pid')).populate('author').exec((err, paper) => {
+			if (err) {
+				return res.negotiate(err)
+			}
+			paper.author.remove(req.param('aid'))
+			paper.save(function(err) {
+				if (err) {
+					console.log(err)
+				}
 
-          if (!files[i + 1]) {
-            u.save(function (err) {
-              if (err) { console.log(err) }
+				return res.json({
+					message: 'Author removed successfully!',
+					paper: paper
+				})
+			})
+		})
+	},
 
-              return res.json({
-                message: files.length + ' file(s) uploaded successfully!',
-                attributes: createdFile.toObject(),
-                paper: files
-              })
-            })
-          }
-        })
-      }
-    })
-  },
+	// addReviewer: Adds a reviewer to a paper
+	addReviewer: function(req, res) {
+		if (!req.param('reid')) return res.json(400, {
+			message: 'Reviewer is not specified.'
+		})
+		Paper.findOne(req.param('pid')).populate('reviewers').exec((err, paper) => {
+			if (err) {
+				return res.negotiate(err)
+			}
+			paper.reviewers.add(req.param('reid'))
+			paper.save(function(err) {
+				if (err) {
+					console.log(err)
+				}
 
-  addAuthor: function (req, res) {
-    if (!req.param('aid')) return res.json(400, {message: 'Author is not specified.'})
-    Paper.findOne(req.param('pid')).populate('author').exec((err, paper) => {
-      if (err) { return res.negotiate(err) }
-      paper.author.add(req.param('aid'))
-      paper.save(function (err) {
-        if (err) { console.log(err) }
+				return res.json({
+					message: 'Reviewer assigned successfully!',
+					paper: paper
+				})
+			})
+		})
+	},
+	
+	// removeReviewer: Removes a reviewer from a paper
+	removeReviewer: function(req, res) {
+		if (!req.param('reid')) return res.json(400, {
+			message: 'Reviewer is not specified.'
+		})
+		Paper.findOne(req.param('pid')).populate('reviewers').exec((err, paper) => {
+			if (err) {
+				return res.negotiate(err)
+			}
+			paper.reviewers.remove(req.param('reid'))
+			paper.save(function(err) {
+				if (err) {
+					console.log(err)
+				}
 
-        return res.json({
-          message: 'Author added successfully!',
-          paper: paper
-        })
-      })
-    })
-  },
-  removeAuthor: function (req, res) {
-    if (!req.param('aid')) return res.json(400, {message: 'Author is not specified.'})
-    Paper.findOne(req.param('pid')).populate('author').exec((err, paper) => {
-      if (err) { return res.negotiate(err) }
-      paper.author.remove(req.param('aid'))
-      paper.save(function (err) {
-        if (err) { console.log(err) }
-
-        return res.json({
-          message: 'Author removed successfully!',
-          paper: paper
-        })
-      })
-    })
-  },
-
-  addReviewer: function (req, res) {
-    if (!req.param('reid')) return res.json(400, {message: 'Reviewer is not specified.'})
-    Paper.findOne(req.param('pid')).populate('reviewers').exec((err, paper) => {
-      if (err) { return res.negotiate(err) }
-      paper.reviewers.add(req.param('reid'))
-      paper.save(function (err) {
-        if (err) { console.log(err) }
-
-        return res.json({
-          message: 'Reviewer assigned successfully!',
-          paper: paper
-        })
-      })
-    })
-  },
-  removeReviewer: function (req, res) {
-    if (!req.param('reid')) return res.json(400, {message: 'Reviewer is not specified.'})
-    Paper.findOne(req.param('pid')).populate('reviewers').exec((err, paper) => {
-      if (err) { return res.negotiate(err) }
-      paper.reviewers.remove(req.param('reid'))
-      paper.save(function (err) {
-        if (err) { console.log(err) }
-
-        return res.json({
-          message: 'Reviewer removed successfully!',
-          paper: paper
-        })
-      })
-    })
-  }
+				return res.json({
+					message: 'Reviewer removed successfully!',
+					paper: paper
+				})
+			})
+		})
+	}
 
 }
--- api/controllers/RatingController.js
+++ api/controllers/RatingController.js
@@ -4,64 +4,82 @@
  * @description :: Server-side logic for managing Ratings
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
 module.exports = {
-  create: function (req, res) {
-    if (!req.param('pid')) return res.json(400, {message: 'Paper is not specified.'})
+	
+	// create: Creates a new rating to the database
+	create: function(req, res) {
+		if (!req.param('pid')) return res.json(400, {
+			message: 'Paper is not specified.'
+		})
 
-    var u = AuthService.user()
+		var u = AuthService.user()
 
-    Paper.findOne({
-      id: req.param('paper_id')
-    }).populate('ratings').exec(function (err, paper) {
-      if (err) return res.json(500, {error: err})
-      if (!paper) return res.json(400, {message: 'Paper not found'})
+		Paper.findOne({
+			id: req.param('paper_id')
+		}).populate('ratings').exec(function(err, paper) {
+			if (err) return res.json(500, {
+				error: err
+			})
+			if (!paper) return res.json(400, {
+				message: 'Paper not found'
+			})
 
-      Rating.create({
-        score: req.param('score')
-      }).exec(function (err, rating) {
-        if (!rating) return res.json(400, {message: 'Rating not found'})
+			Rating.create({
+				score: req.param('score')
+			}).exec(function(err, rating) {
+				if (!rating) return res.json(400, {
+					message: 'Rating not found'
+				})
 
-        paper.ratings.add(rating.id)
-        u.ratings.add(rating.id)
-        u.save()
-        paper.save((err) => {
-          if (err) return res.json(500, {error: err})
-          return res.json({
-            message: '',
-            rating: rating,
-            paper: paper
-          })
-        })
-      })
-    })
-  },
+				paper.ratings.add(rating.id)
+				u.ratings.add(rating.id)
+				u.save()
+				paper.save((err) => {
+					if (err) return res.json(500, {
+						error: err
+					})
+					return res.json({
+						message: '',
+						rating: rating,
+						paper: paper
+					})
+				})
+			})
+		})
+	},
+	
+	// update: Updates a rating in the database
+	update: function(req, res) {
+		var u = AuthService.user()
 
-  update: function (req, res) {
-    var u = AuthService.user()
+		Paper.findOne({
+			id: req.param('paper_id')
+		}).populate('ratings').exec(function(err, paper) {
+			if (err) return res.json(500, {
+				error: err
+			})
+			if (!paper) return res.json(400, {
+				message: 'Paper not found'
+			})
 
-    Paper.findOne({
-      id: req.param('paper_id')
-    }).populate('ratings').exec(function (err, paper) {
-      if (err) return res.json(500, {error: err})
-      if (!paper) return res.json(400, {message: 'Paper not found'})
-
-      // if not work delete populate
-      console.log(paper.ratings)
-
-      Rating.update({
-        id: req.param('rating_id')
-      }, {
-        score: req.param('score')
-      }).exec(function (err, rating) {
-        if (err) return res.json(500, {error: err})
-        if (!rating) return res.json(400, {message: 'Rating not found'})
-        return res.json({
-          message: '',
-          rating: rating,
-          paper: paper
-        })
-      })
-    })
-  }
+			Rating.update({
+				id: req.param('rating_id')
+			}, {
+				score: req.param('score')
+			}).exec(function(err, rating) {
+				if (err) return res.json(500, {
+					error: err
+				})
+				if (!rating) return res.json(400, {
+					message: 'Rating not found'
+				})
+				return res.json({
+					message: '',
+					rating: rating,
+					paper: paper
+				})
+			})
+		})
+	}
+	
 }
