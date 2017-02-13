9b28f8e3f1a192ac99c9c29211b494fdd9e8797a
Rinnegatamante <rinnegatamante@gmail.com>
2017-02-13 11:19:58 +0100

    Idented and commented addchairs.
    Idented and commented addchairs.

--- assets/conferences/addchairs.controller.js
+++ assets/conferences/addchairs.controller.js
@@ -1,29 +1,38 @@
-app.controller('addchairsController',
-  ($scope, $http, $location, $routeParams) => {
-    $scope.conf = {
-      id: $routeParams.cid
-    }
-    $http.post('/conference/getData', $scope.conf).then(res => {
-      $scope.conf = res.data.conference
-    })
-    $scope.$watch('user.field', function (newVal, oldVal) {
-      if (newVal != oldVal) {
-        $http.post('/user/searchByName', $scope.user).then(res => {
-          $scope.users = res.data.users
-        })
-      }
-    })
-    $scope.add = function (id) {
-      $scope.conf.add_id = id
-      $http.post('/conference/addChair', $scope.conf).then(res => {
-        $scope.conf = res.data.conference
-      })
-    }
-    $scope.delete = function (id) {
-      $scope.conf.delete_id = id
-      $http.post('/conference/deleteChair', $scope.conf).then(res => {
-        $scope.conf = res.data.conference
-      })
-    }
-  }
-)
+// Controller for addchairs template
+app.controller('addchairsController',($scope, $http, $location, $routeParams) => {
+	
+	// Get conference ID from URL
+	$scope.conf = {
+		id: $routeParams.cid
+	}
+	
+	// Request conference data
+	$http.post('/conference/getData', $scope.conf).then(res => {
+		$scope.conf = res.data.conference
+	})
+	
+	// Watch for search input status modifications
+	$scope.$watch('user.field', function (newVal, oldVal) {
+		if (newVal != oldVal) { // Modification detected, request a search by name
+			$http.post('/user/searchByName', $scope.user).then(res => {
+				$scope.users = res.data.users
+			})
+		}
+	})
+	
+	// add function, adds a new co-chair to the conference
+	$scope.add = function (id) {
+		$scope.conf.add_id = id
+		$http.post('/conference/addChair', $scope.conf).then(res => {
+			$scope.conf = res.data.conference
+		})
+	}
+	
+	// delete function, removes a co-chair from the conference
+	$scope.delete = function (id) {
+		$scope.conf.delete_id = id
+		$http.post('/conference/deleteChair', $scope.conf).then(res => {
+			$scope.conf = res.data.conference
+		})
+	}
+})
